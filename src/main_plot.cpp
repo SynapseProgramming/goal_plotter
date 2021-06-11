14f58eba71c221e125c7179b48b2d39157f82d47
Roald Ong <roald.ong@gmail.com>
2021-06-11 14:31:37 +0800

    Added in goal_map to store goal values, and added in basic ui functions.

--- src/main_plot.cpp
+++ src/main_plot.cpp
@@ -1,33 +1,67 @@
 #include <chrono>
 #include <memory>
 #include <iostream>
+#include <string>
+#include <map>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int32.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
 #define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 
+struct goal{
+    double x;
+    double y;
+    double z;
+    double w;
+};
+
+
+
 class plot : public rclcpp::Node
 {
 public:
-
   plot()
   : Node("goal_plotter")
   {
-     auto goal_callback= [this](const geometry_msgs::msg::PoseStamped::SharedPtr msg){
+    auto goal_callback= [this](const geometry_msgs::msg::PoseStamped::SharedPtr msg){
        std::cout<<"goal received: "<<msg->pose.position.x<<" "<<msg->pose.position.y<<" ";
        std::cout<<msg->pose.orientation.z<<" "<<msg->pose.orientation.w<<"\n";
     };
 
-    //publisher_ = this->create_publisher<std_msgs::msg::Int32>("result", 10);
-    subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("goal_pose",10,goal_callback);
-
+    goal_subscriber = this->create_subscription<geometry_msgs::msg::PoseStamped>("goal_pose",10,goal_callback);
+    main_menu();
   }
-private:
+    void main_menu(){
+      std::string input;
+      std::cout<<"(ag) Add a new goal\n";
+      std::cout<<"(ls) List all goals\n";
+      std::cout<<"(rm) Remove a specific goal\n";
+      std::cout<<"(ck) Check if a goal exists\n";
+      std::cout<<"(sv) Save and export goal file\n";
 
-  //shared ptr of a timer
-  //rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr subscription_;
+      std::cin>>input;
+      if(input=="ag"){new_goal();}
+      else{std::cout<<"Please re-enter one of the aforementioned acronyms.\n";main_menu();}
+    }
+    void new_goal(){
+      std::cout<<"Please enter a name for your goal.\n";
+      std::string goal_name;
+      std::cin>>goal_name;
+      if(wait_confirmation()) std::cout<<goal_name<<"\n";
+      else main_menu();
+
+    }
+    // the wait_confirmation function would prompt the user to enter (y\n). Function returns true if (y) and false if (anything else if entered)
+    bool wait_confirmation(){
+      std::cout<<"Please enter (y) for yes. Press and enter anything else for no.\n";
+      std::string input;
+      std::cin >> input;
+      return input=="y" ? true: false;
+    }
+private:
+  std::map<std::string,goal> goal_map;
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_subscriber;
+
 
 };
 
