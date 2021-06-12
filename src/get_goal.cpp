7820c847f4c4515d5e94f402d3dc13afd77638f5
Roald Ong <roald.ong@gmail.com>
2021-06-12 17:47:24 +0800

    Added in service server to send selected goal locations.

--- src/get_goal.cpp
+++ src/get_goal.cpp
@@ -5,7 +5,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "goal_plotter/goal.h"
-
+#include "goal_plotter/srv/sgoal.hpp"
 
 
 class get_goal : public rclcpp::Node
@@ -20,22 +20,35 @@ public:
     selected_goal.w=0;
 
     auto goal_callback= [this](const geometry_msgs::msg::PoseStamped::SharedPtr msg){
-       std::cout<<"goal received: "<<msg->pose.position.x<<" "<<msg->pose.position.y<<" ";
-       std::cout<<msg->pose.orientation.z<<" "<<msg->pose.orientation.w<<"\n";
+
+       RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"[get_goal] Received goal values: x: %f y: %f z: %f w: %f",msg->pose.position.x,msg->pose.position.y,msg->pose.orientation.z,msg->pose.orientation.w);
        selected_goal.x=msg->pose.position.x;
        selected_goal.y=msg->pose.position.y;
        selected_goal.z=msg->pose.orientation.z;
        selected_goal.w=msg->pose.orientation.w;
-       //TODO: create a service which would return the current selected goal value
     };
 
+    auto send_goal= [this](const std::shared_ptr<goal_plotter::srv::Sgoal::Request> request, const std::shared_ptr<goal_plotter::srv::Sgoal::Response> response){
+          data=request->data;
+           // send over the selected goal.
+          response->x = selected_goal.x;
+           response->y = selected_goal.y;
+           response->z = selected_goal.z;
+           response->w = selected_goal.w;
+
+           RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sent goal values");
+        };
+
     goal_subscriber = this->create_subscription<geometry_msgs::msg::PoseStamped>("goal_pose",10,goal_callback);
+    goal_service = this->create_service<goal_plotter::srv::Sgoal>("get_curr_goal", send_goal);
   }
+
 private:
 
 rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_subscriber;
+rclcpp::Service<goal_plotter::srv::Sgoal>::SharedPtr goal_service;
 goal_plotter::goal selected_goal;
-
+bool data=false;
 };
 
 
