6187de578489f228e08778f59cf4c77e274f0f7c
Roald Ong <roald.ong@gmail.com>
2021-06-13 00:51:12 +0800

    Added in shared_plot_from_this function which returns a shared_ptr to the current object.

--- src/main_plot.cpp
+++ src/main_plot.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "goal_plotter/goal.h"
+#include "goal_plotter/srv/sgoal.hpp"
 
 #define be RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT
 
@@ -17,8 +18,14 @@ public:
   : Node("goal_plotter")
   {
 
-    main_menu();
+    sub_goal_client= this->create_client<goal_plotter::srv::Sgoal>("get_curr_goal");
   }
+
+  // this function returns a shared_ptr which points to the current instant.
+   std::shared_ptr<plot> shared_plot_from_this(){
+     return std::static_pointer_cast<plot>((shared_from_this()));
+   }
+
     void main_menu(){
       std::string input;
       std::cout<<"(ag) Add a new goal\n";
@@ -29,8 +36,11 @@ public:
 
       std::cin>>input;
       if(input=="ag"){new_goal();}
-      else{std::cout<<"Please re-enter one of the aforementioned acronyms.\n"; rclcpp::shutdown();}
-    }
+      else{
+        std::cout<<"Please re-enter one of the aforementioned acronyms.\n";
+         main_menu();
+        }
+      }
     // This function would add a new goal into the map. With a custom goal_name
     void new_goal(){
       std::cout<<"Please enter a name for your goal.\n";
@@ -38,7 +48,9 @@ public:
       std::cin>>goal_name;
       if(wait_confirmation()){
        //TODO: when the user confirms that he wants this goal location,  call the service which
-       // gets the current selected goal value and stores it inside a map
+       // gets the current selected goal value and stores it inside a map.
+       std::shared_ptr<plot> current_node_ptr= shared_plot_from_this();
+       std::cout<<"this is the current number of active shared_ptr "<<current_node_ptr.use_count()<<"\n";
 
       }
       else main_menu();
@@ -54,13 +66,15 @@ public:
     }
 private:
   std::map<std::string,goal_plotter::goal> goal_map;
-
+  rclcpp::Client<goal_plotter::srv::Sgoal>::SharedPtr sub_goal_client;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<plot>());
+  std::shared_ptr<plot> node_ptr=std::make_shared<plot>();
+  node_ptr->main_menu();
+  rclcpp::spin(node_ptr);
   rclcpp::shutdown();
   return 0;
 }
