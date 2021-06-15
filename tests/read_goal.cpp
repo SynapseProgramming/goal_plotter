2921fd24eb4bf6dd9ab731ecde8fe5ac55f7acd4
Roald Ong <roald.ong@gmail.com>
2021-06-15 14:37:58 +0800

    removed unused code.

--- tests/read_goal.cpp
+++ tests/read_goal.cpp
@@ -8,71 +8,6 @@
 #include "rapidjson/reader.h"
 #include "rapidjson/stringbuffer.h"
 
-/*
-struct MyHandler {
-  bool Null() {
-    std::cout << "Null()" << std::endl;
-    return true;
-  }
-  bool Bool(bool b) {
-    std::cout << "Bool(" << std::boolalpha << b << ")" << std::endl;
-
-    return true;
-  }
-  bool Int(int i) {
-    std::cout << "Int(" << i << ")" << std::endl;
-    return true;
-  }
-  bool Uint(unsigned u) {
-    std::cout << "Uint(" << u << ")" << std::endl;
-    return true;
-  }
-  bool Int64(int64_t i) {
-    std::cout << "Int64(" << i << ")" << std::endl;
-    return true;
-  }
-  bool Uint64(uint64_t u) {
-    std::cout << "Uint64(" << u << ")" << std::endl;
-    return true;
-  }
-  bool Double(double d) {
-    std::cout << "Double(" << d << ")" << std::endl;
-    return true;
-  }
-  bool RawNumber(const char* str, rapidjson::SizeType length, bool copy) {
-    std::cout << "Number(" << str << ", " << length << ", " << std::boolalpha
-              << copy << ")" << std::endl;
-    return true;
-  }
-  bool String(const char* str, rapidjson::SizeType length, bool copy) {
-    std::cout << "String(" << str << ", " << length << ", " << std::boolalpha
-              << copy << ")" << std::endl;
-    return true;
-  }
-  bool StartObject() {
-    std::cout << "StartObject()" << std::endl;
-    return true;
-  }
-  bool Key(const char* str, rapidjson::SizeType length, bool copy) {
-    std::cout << "Key(" << str << ", " << length << ", " << std::boolalpha
-              << copy << ")" << std::endl;
-    return true;
-  }
-  bool EndObject(rapidjson::SizeType memberCount) {
-    std::cout << "EndObject(" << memberCount << ")" << std::endl;
-    return true;
-  }
-  bool StartArray() {
-    std::cout << "StartArray()" << std::endl;
-    return true;
-  }
-  bool EndArray(rapidjson::SizeType elementCount) {
-    std::cout << "EndArray(" << elementCount << ")" << std::endl;
-    return true;
-  }
-};
-*/
-
 int main() {
   rapidjson::Reader reader;
   // MyHandler handler;
@@ -100,14 +35,6 @@ int main() {
       }
       std::cout << "\n";
     }
-
-    /*
-    std::string input = "{ \"test\": true }";
-
-    char json[input.length() + 1];
-    std::strcpy(json, input.c_str());
-    rapidjson::StringStream ss(json);
-    reader.Parse(ss, handler);*/
   }
 
   else {
