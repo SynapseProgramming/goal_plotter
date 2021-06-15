601c8fbfeeacfa83f69f4b06cf4815590e0bcfe1
Roald Ong <roald.ong@gmail.com>
2021-06-15 09:39:54 +0800

    Successfully added in rapidjson library and added in json write test program.

---
+++ tests/write_json.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "rapidjson/reader.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+using namespace std;
+
+int main() {
+  std::cout << "test\n";
+
+  return 0;
+}
