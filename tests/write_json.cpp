1b5490e9b9b1a95f48573f2a243c8002233fa5fa
Roald Ong <roald.ong@gmail.com>
2021-06-15 10:06:58 +0800

    added in json writer example program

--- tests/write_json.cpp
+++ tests/write_json.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 
-#include "rapidjson/reader.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
+
+using namespace rapidjson;
 using namespace std;
 
 int main() {
-  std::cout << "test\n";
+  StringBuffer s;
+  Writer<StringBuffer> writer(s);
 
+  writer.StartObject();
+  writer.Key("hello");
+  writer.String("world");
+  writer.Key("t");
+  writer.Bool(true);
+  writer.Key("f");
+  writer.Bool(false);
+  writer.Key("n");
+  writer.Null();
+  writer.Key("i");
+  writer.Uint(123);
+  writer.Key("pi");
+  writer.Double(3.1416);
+  writer.Key("a");
+  writer.StartArray();
+  for (unsigned i = 0; i < 4; i++) writer.Uint(i);
+  writer.EndArray();
+  writer.EndObject();
+
+  cout << s.GetString() << endl;
   return 0;
 }
