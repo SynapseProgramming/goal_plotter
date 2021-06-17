ce379e377ada1894a5c756267a81d5b0f171337b
Roald Ong <roald.ong@gmail.com>
2021-06-17 13:42:19 +0800

    Added in tkinter examples for placing a button + a drop down list.

--- scripts/py_test.py
+++ scripts/py_test.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 ...
 
-print("test")
+import tkinter  # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
+
+
+top = tkinter.Tk()
+top.geometry("700x800")
+
+GOALS = ["g1", "g2", "g3"]
+
+# selected goal is a string used to store the currenty selected drop down
+selected_goal = tkinter.StringVar(top)
+selected_goal.set(GOALS[0])
+
+# create options menu widget
+goal_menu = tkinter.OptionMenu(top, selected_goal, *GOALS)
+goal_menu.place(x=400, y=300)
+
+
+def send_goal_callback():
+    print(selected_goal.get())
+
+
+send_goal_button = tkinter.Button(top, text="send_goal", command=send_goal_callback)
+send_goal_button.place(x=100, y=200)
+top.mainloop()
