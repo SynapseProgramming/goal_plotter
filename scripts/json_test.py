e1c0577f6bc05c978657cb9392d47b93f191a59b
Roald Ong <roald.ong@gmail.com>
2021-06-17 14:10:11 +0800

    Added in a program which is able to read goal data from a json file.

---
+++ scripts/json_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+...
+
+import json
+
+goal_file = open(
+    "/home/ro/dev_ws/install/goal_plotter/share/goal_plotter/goal_json/goal_test.json"
+)
+
+goal_map = json.load(goal_file)
+
+for goal_name, goal_location in goal_map.items():
+    print(goal_name)
+    gx = goal_location[0]
+    gy = goal_location[1]
+    gz = goal_location[2]
+    gw = goal_location[3]
+    print(gx, gy, gz, gw)
