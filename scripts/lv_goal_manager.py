c26530d65252b860d75a9849bf3bf9eebcfb9087
Roald Ong <roald.ong@gmail.com>
2021-07-14 14:37:40 +0800

    Added in class to send goals to the navigation stack

--- scripts/lv_goal_manager.py
+++ scripts/lv_goal_manager.py
@@ -8,6 +8,69 @@ from std_msgs.msg import Int32
 import rclpy
 import json
 from rclpy.node import Node
+from rclpy.action import ActionClient
+
+
+class goto_pose:
+    def __init__(self, action_client):
+        self._action_client = action_client
+        self.goal_status = False
+        self.goal_accept_status = False
+        self.failure_flag = False
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            print("nav2 Goal Rejected")
+            self.goal_accept_status = False
+            return
+
+        print("nav2 Goal accepted")
+        self.goal_accept_status = True
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+
+        result = future.result().result
+        status = future.result().status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            print("navigation succeeded")
+            self.goal_status = True
+        else:
+            print("navigation failed!")
+            self.goal_status = False
+            self.failure_flag = True
+
+        # send_goal would accept an array as a goal pose
+
+    def send_goal(self, goal_pose):
+        print("Waiting for action server")
+        self._action_client.wait_for_server()
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose.pose.position.x = goal_pose[0]
+        goal_msg.pose.pose.position.y = goal_pose[1]
+        goal_msg.pose.pose.orientation.z = goal_pose[2]
+        goal_msg.pose.pose.orientation.w = goal_pose[3]
+        # fill up the rest later
+        print("Sending goal request")
+
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def get_status(self):
+        status = {}
+        status["gs"] = self.goal_status
+        status["gas"] = self.goal_accept_status
+        status["f_flag"] = self.failure_flag
+        return status
+
+    def reset_status(self):
+        self.goal_status = False
+        self.goal_accept_status = False
+        self.failure_flag = False
 
 
 class ros2_main(Node):
@@ -35,15 +98,20 @@ class ros2_main(Node):
         )
         self.status_publisher = self.create_publisher(Int32, "goal_state", 10)
         self.subscription
+        self.goto_pose_ = goto_pose(
+            ActionClient(self, NavigateToPose, "navigate_to_pose")
+        )
 
     def timed_callback(self):
+        # update statuses
+        nav2_status = self.goto_pose_.get_status()
         # send a goal to nav stack when the send_goal is true and there is a valid goal
         if (
             (self.goal_name_ in self.goal_map_)
             and self.current_state_ == 0
             and self.send_goal_ == True
         ):
-            print("STONKS")
+            self.goto_pose_.send_goal(self.goal_map_[self.goal_name_])
             self.current_state_ = 1
         self.pub_status(self.current_state_)
 
