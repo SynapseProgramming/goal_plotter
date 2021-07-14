29f0e33eb3f42850a6532a3b3697a5b11ba114e4
Roald Ong <roald.ong@gmail.com>
2021-07-14 12:05:32 +0800

    Added in code to read in the saved goals from json file.

---
+++ launch/goal_manager.launch.py
@@ -0,0 +1,28 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # goal file to load poses from
+    load_goal_file = "goal_test.json"
+
+    load_file_path = os.path.join(
+        get_package_share_directory("goal_plotter"), "goal_json", load_goal_file
+    )
+    run_goal_manager = Node(
+        package="goal_plotter",
+        executable="lv_goal_manager.py",
+        name="goal_manager",
+        emulate_tty=True,
+        parameters=[{"load_file_path": load_file_path}],
+        output="screen",
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(run_goal_manager)
+
+    return ld
--- scripts/lv_goal_manager.py
+++ scripts/lv_goal_manager.py
@@ -4,6 +4,7 @@
 from goal_plotter.msg import Goalactions
 
 import rclpy
+import json
 from rclpy.node import Node
 
 
@@ -13,6 +14,18 @@ class ros2_main(Node):
         self.goal_name_ = str()
         self.send_goal_ = False
         self.cancel_goal_ = False
+        self.declare_parameter("load_file_path")
+        self.goal_file_path = (
+            self.get_parameter("load_file_path").get_parameter_value().string_value
+        )
+        self.goal_file_ = open(self.goal_file_path)
+
+        # code to get json dict
+        self.goal_map_ = json.load(self.goal_file_)
+        self.goal_names_ = self.goal_map_.keys()
+        # TODO: remove test code later
+        for x in self.goal_map_:
+            print(str(x))
         self.subscription = self.create_subscription(
             Goalactions, "goal_actions", self.update_actions, 10
         )
